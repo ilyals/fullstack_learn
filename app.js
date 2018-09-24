42feca162065cf5d6d05483f5d41fa1fe3c8adc2
ilya <ilya@luskanov.com>
2018-09-24 10:25:43 -0400

    Start - Block3

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+//Required
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // Routes
 const authRoutes = require('./routes/auth');
@@ -8,10 +12,29 @@ const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
+//Config
+const keys = require('./config/keys');
+
+//Application
+const app = express();
+
+//Database
+mongoose.connect(keys.mongoURI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(error => console.log(error))
+
+//Required
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
 
+//Export app
 module.exports = app;
